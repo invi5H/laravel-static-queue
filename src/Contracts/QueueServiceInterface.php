66c6c6d5df99f6fdd6979a0d377b14535f014e6c
Mridul Aggarwal <mridul89@users.noreply.github.com>
2022-06-02 13:30:33 +0530

    return type hints

--- src/Contracts/QueueServiceInterface.php
+++ src/Contracts/QueueServiceInterface.php
@@ -2,6 +2,8 @@
 
 namespace Invi5h\LaravelStaticQueue\Contracts;
 
+use Invi5h\LaravelStaticQueue\Job;
+
 interface QueueServiceInterface
 {
     /**
@@ -10,45 +12,45 @@ interface QueueServiceInterface
      * string - job class name resolved via the default service container or a standard php function name
      * callable - any standard php callable such as closure, arrow function, array syntax, class@method syntax, invokable object.
      */
-    public function job(string|callable|array|object $job, string $queue = null);
+    public function job(string|callable|array|object $job, string $queue = null) : void;
 
     /**
      * Alias to the above.
      */
-    public function push(string|callable|array|object $job, string $queue = null);
+    public function push(string|callable|array|object $job, string $queue = null) : void;
 
     /**
      * Pushes to a specific queue.
      */
-    public function pushOn(string $queue, string|callable|array|object $job);
+    public function pushOn(string $queue, string|callable|array|object $job) : void;
 
     /**
      * Bulk pushes a list of jobs to the same queue.
      */
-    public function bulk(array $jobs, string $queue = null);
+    public function bulk(array $jobs, string $queue = null) : void;
 
     /**
      * Resets the queue to the given set of jobs.
      */
-    public function setQueue(string $queue, array $jobs);
+    public function setQueue(string $queue, array $jobs) : void;
 
     /**
      * Clears the given queue.
      */
-    public function clear(string $queue = null);
+    public function clear(string $queue = null) : void;
 
     /**
      * Gets all the jobs for the given queue.
      */
-    public function getJobs(string $queue = null);
+    public function getJobs(string $queue = null) : array;
 
     /**
      * Gets the number of jobs for the given queue.
      */
-    public function size(string $queue = null);
+    public function size(string $queue = null) : int;
 
     /**
      * Gets the next job in the given queue.
      */
-    public function pop(string $queue = null);
+    public function pop(string $queue = null) : Job;
 }
